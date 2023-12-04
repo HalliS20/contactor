98c0a322e77d9e519c431d83d627466c78c41c18
HalliS2o <hallist@hey.com>
2023-12-04 11:13:38 +0000

    did some work on linting configs

---
+++ src/views/Main/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { ScrollView, Text, View } from "react-native"
+
+const Main = ({ navigation: { navigate } }) => (
+    <View>
+        <Text>Contactor</Text>
+        <ScrollView>
+            <Text>Hello World</Text>
+        </ScrollView>
+    </View>
+)
+
+export default Main
