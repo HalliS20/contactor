29672adce598ad8770762d2c26992399636843a0
HalliS2o <hallist@hey.com>
2023-12-04 15:36:33 +0000

    finished getting filenames with contact concept

--- src/dummies/dummyData.js
+++ src/dummies/dummyData.js
@@ -1,67 +1,40 @@
-import  React, { useState, useEffect } from "react"
-import { ScrollView, Text, View } from "react-native"
-import { addContact, getAllContacts, getOneContract, removeContact,cleanDirectory } from "../services/fileService";
+import React, {useState, useEffect} from "react"
+import {ScrollView, Text, View} from "react-native"
+import {
+    addContact,
+    getAllContacts,
+    cleanDirectory,
+} from "../services/fileService"
 import ContactList from "../components/ContactList"
+import data from "./data.js"
 
-
-
-const userData = {
-    name: "John Doe",
-    phone: "555-555-5555",
-    photo: "https://picsum.photos/200/300",
-
-}
-const userData2 = {
-    name: "Jane Does",
-    phone: "555-555-5555",
-    photo: "https://picsum.photos/200/300",
-
-}
-
-const userData3 = {
-    name: "My Name",
-    phone: "555-555-5555",
-    photo: "https://picsum.photos/200/300",
-
-}
-
-const userData4 = {
-    name: "apiame",
-    phone: "555-555-5555",
-    photo: "https://picsum.photos/200/300",
-
-}
-
-const Dummy = () => { 
-    const [contacts, setContacts] = useState([]);
+const Dummy = () => {
+    const [contacts, setContacts] = useState([])
+    const users = data()
     useEffect(() => {
-        addContact(userData.name, JSON.stringify(userData));
-        addContact(userData2.name, JSON.stringify(userData2));
-        addContact(userData3.name, JSON.stringify(userData3));
-        addContact(userData4.name, JSON.stringify(userData4));
-        
+        const add = false // change to true to add the data from data.js
+        const clean = false // change to true to clean the directory
 
-        getAllContacts().then(contacts => {
-            const contactsObjects = contacts.map(contact => JSON.parse(contact));
-            setContacts(contactsObjects);
-            console.log("This is the contact info:..",contactsObjects)
-        });
-    }, []);
+        if (add === true) {
+            for (let i = 0; i < users.length; i++) {
+                addContact(users[i].name, JSON.stringify(users[i]))
+            }
+        }
+        if (clean === true) {
+            cleanDirectory()
+        }
 
+        getAllContacts().then((contacts) => {
+            setContacts(contacts)
+        })
+    }, [])
 
-    
+    return (
+        <View>
+            <Text> Hollo Welt </Text>
+            <ContactList contacts={contacts} />
+        </View>
+    )
+}
 
-        
-        return (
-            <View>
-                <Text> Hollo Welt </Text>
-                    <ContactList contacts={contacts} />
-                    
-
-            </View>
-        )
-        
-    }
-    
-export default Dummy;
-
+export default Dummy
