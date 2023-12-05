dd69d70d2c8c4d7ba0c6e48663ef366bbdbf2aa1
Ingi Kr <ingik21@ru.is>
2023-12-05 16:12:05 +0000

    styleNavyAndGold

--- src/views/ContactView/index.js
+++ src/views/ContactView/index.js
@@ -1,53 +1,64 @@
 import React, {useState} from "react"
-import {View, Text, Button, Linking, Image} from "react-native"
+import {View, Text, Button, Linking, Image, Pressable} from "react-native"
 import {useNavigation} from "@react-navigation/native"
 import {removeContact} from "../../services/fileService"
 import {deviceWidth} from "../../styles/sizes"
+import styles from "./styles"
 
 function ContactView({route}) {
     const {contact} = route.params
     const navigation = useNavigation()
-    const [imageSource, setImageSource] = useState(
-        contact.image
-            ? {uri: contact.image}
-            : require("../../resources/default-avatar.png"),
-    )
+    const [imageFound, setImageFound] = useState(true)
 
     const handleCall = () => {
         Linking.openURL(`tel:${contact.phone}`)
     }
-
-    const handleError = () => {
-        setImageSource(require("../../resources/default-avatar.png"))
-    }
-
+    {console.log(contact.image)}
     return (
-        <View>
-            <Image
-                style={{width: deviceWidth * 1, height: 100}}
-                source={imageSource}
-                onError={handleError}
-                defaultSource={require("../../resources/default-avatar.png")}
-            />
-            {console.log(contact.image)}
-            <Text>Name: {contact.name}</Text>
-            <Text>Phone: {contact.phone}</Text>
-            <Text>fileName: {contact.fileName}</Text>
-            <Button title="Call" onPress={handleCall} />
-            <Text
-                onPress={() => {
+        <View style={styles.container}>
+            {contact.image && imageFound && (
+                <Image
+                    style={{width: deviceWidth * 1, height: 200}}
+                    source={{uri: contact.image}}
+                    onError={() => setImageFound(false)}
+                />
+            )}
+
+            <Text style={styles.textStyle}>
+            Name: {contact.name}</Text>
+            <Text style={styles.textStyle}>
+            Phone: {contact.phone}</Text>
+
+
+            <Pressable 
+            style={styles.buttonStyleCall}
+            onPress={handleCall}>
+            <Text style={styles.textStyleCall}>Call me</Text>
+             </Pressable>
+            <View style={styles.buttonRow}>
+             <Pressable
+            style={styles.buttonStyle}
+            onPress={() => navigation.navigate('ContactForm', {contact})}>
+                <Text>Edit me?</Text>
+            </Pressable>
+            <Pressable                
+            style={styles.buttonStyleDelete}
+            onPress={() => {
                     removeContact(contact.fileName)
                     navigation.goBack()
-                }}
-            >
-                Delete me
-            </Text>
-            <Button
-                title="Edit Contact"
-                onPress={() => navigation.navigate("ContactForm", {contact})}
-            />
-            <Button title="Back to Main" onPress={() => navigation.goBack()} />
-        </View>
+                }}>
+                    <Text
+                    style={styles.textStyleDelete}
+                    >Delete Me!!!</Text>
+            </Pressable>
+            </View>
+            
+            <Pressable 
+            style={styles.buttonStyleBack}
+            onPress={() => navigation.goBack()}>
+                <Text>Back to Main</Text>
+            </Pressable>        
+            </View>
     )
 }
 
