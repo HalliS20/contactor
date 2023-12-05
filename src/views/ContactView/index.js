571603c1a54d8ead245885dc24eda06bdc18867f
HalliS2o <hallist@hey.com>
2023-12-05 10:11:31 +0000

    phonacall made

--- src/views/ContactView/index.js
+++ src/views/ContactView/index.js
@@ -1,23 +1,30 @@
 import React from "react"
-import {View, Text, Button} from "react-native"
+import {View, Text, Button, Linking} from "react-native"
 import {useNavigation} from "@react-navigation/native"
 import {removeContact} from "../../services/fileService"
 
-
 function ContactView({route}) {
-    const { contact } = route.params;
+    const {contact} = route.params
     const navigation = useNavigation()
+
+    const handleCall = () => {
+        Linking.openURL(`tel:${contact.phone}`)
+    }
 
     return (
         <View>
             <Text>Name: {contact.name}</Text>
-            <Text>Email: {contact.email}</Text>
             <Text>Phone: {contact.phone}</Text>
             <Text>fileName: {contact.fileName}</Text>
-            <Text onPress={() => {
-                removeContact(contact.fileName);
-                navigation.goBack();
-                }}>Delete me</Text>
+            <Button title="Call" onPress={handleCall} />
+            <Text
+                onPress={() => {
+                    removeContact(contact.fileName)
+                    navigation.goBack()
+                }}
+            >
+                Delete me
+            </Text>
             <Button title="Back to Main" onPress={() => navigation.goBack()} />
         </View>
     )
